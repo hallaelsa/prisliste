50c183ad6edca03a4ce350bb797d77b479fc82bf
miina <hallaelsa@hotmail.com>
2018-05-26 11:02:46 +0200

    apk works!!!!!!!!!!!!!!!!

--- components/redux/reducers.js
+++ components/redux/reducers.js
@@ -6,50 +6,12 @@ function initState() {
 		dateLimit: 3,
 		items: [
 			{
-				item: "Melkesjokolade", itemInfo: [
-					{ storeName: "Rema", price: "30", date: "10.03.2018" },
-					{ storeName: "Kiwi", price: "25", date: "10.12.2017" },
-					{ storeName: "Joker", price: "33", date: "10.12.2016" },
-					{ storeName: "Meny", price: "50", date: "10.11.2017" },
-					{ storeName: "Coop", price: "42", date: "10.12.2016" },
-					{ storeName: "Extra", price: "50", date: "10.10.2017" },
-					{ storeName: "Europris", price: "42", date: "10.02.2018" },
-					{ storeName: "Coop prix", price: "50", date: "10.09.2017" },
-					{ storeName: "Coop mega", price: "42", date: "10.01.2018" },
-				]
-			},
-			{
-				item: "Vasa knekkebrod", itemInfo: [
-					{ storeName: "Joker", price: "10", date: "10.03.2018" },
-					{ storeName: "Kiwi", price: "12", date: "10.12.2017" },
-				]
-			},
-			{
-				item: "Salmalaks", itemInfo: [
-					{ storeName: "Joker", price: "100", date: "10.03.2018" },
-					{ storeName: "Kiwi", price: "120", date: "10.09.2017" },
-				]
-			},
-			{
-				item: "Druer", itemInfo: [
-					{ storeName: "Joker", price: "32", date: "10.03.2018" },
-					{ storeName: "Kiwi", price: "27", date: "10.08.2017" },
-					{ storeName: "Meny", price: "25", date: "10.12.2017" },
-					{ storeName: "Rema 1000", price: "33", date: "10.12.2016" },
-				]
-			},
-			{
 				item: "Banan", itemInfo: [
 					{ storeName: "Joker", price: "2", date: "10.03.2018" },
 					{ storeName: "Kiwi", price: "5", date: "10.10.2017" },
 				]
 			},
-			{
-				item: "vaskepulver", itemInfo: [
-					{ storeName: "Joker", price: "80", date: "10.03.2018" },
-					{ storeName: "Kiwi", price: "50", date: "10.10.2017" },
-				]
-			},
+			
 			{
 				item: "Gulrot", itemInfo: [
 					{ storeName: "Joker", price: "30", date: "10.03.2018" },
@@ -64,12 +26,6 @@ function initState() {
 					{ storeName: "Kiwi", price: "24", date: "10.12.2017" },
 				]
 			},
-			{
-				item: "Melis", itemInfo: [
-					{ storeName: "Joker", price: "10", date: "10.03.2018" },
-					{ storeName: "Kiwi", price: "18", date: "10.12.2017" },
-				]
-			},
 		]
 	};
 }
@@ -81,6 +37,9 @@ function saveItems(items) {
 function reducer(state = initState(), action) {
 	switch (action.type) {
 		case "SET_ITEMS": {
+			if(!action.items) {
+				return state;
+			}
 			let nextState = Object.assign({}, state)
 			nextState.items = action.items;
 			return nextState;
